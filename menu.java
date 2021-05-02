bfe86942993be84894f171f43b56589bdd52e21d
Tim <timothyomahony@gmail.com>
2021-05-02 18:22:07 +0100

    added new stuff to menu :)

--- menu.java
+++ menu.java
@@ -1,7 +1,7 @@
 package assignment;
 import java.util.*;
 public class menu {
-    public static boolean mainMenu(int start) throws Exception{
+    public static boolean mainMenu(int start){
         if (start == 1){
             return true;
         }
@@ -9,7 +9,8 @@ public class menu {
             return false;
         }
         else {
-            throw new Exception("Please enter either 'start' or 'quit'");
+            System.out.println("Please enter either '1' for start or '2' for quit");
+            return false;
         }
     }
 
@@ -20,8 +21,7 @@ public class menu {
     }
 
 
-    public static void rockPaperScissors(int start) throws Exception {
-        //Initialize the Scanner and print a welcome message
+    public static void rockPaperScissors(int start) {
         Scanner RPSin = new Scanner(System.in);
         System.out.println("Welcome to Rock, Paper, Scissors!");
         //Use a while(true) loop and only break the loop if the user wants to quit
@@ -31,16 +31,14 @@ public class menu {
             String playerMove = RPSin.next();
             if (playerMove.equals("quit")) {
                 System.out.println("Thanks for playing rock paper scissors :)");
-                mainMenu(start);
+                gameChoice();
                 break;
             }
-//
 //            //Check if the user's move is valid (rock, paper, or scissors)
 //            if(!(playerMove == 1) && !(playerMove == 2) && !(playerMove == 3)){
             if (!playerMove.equals("rock") && !playerMove.equals("paper") && !playerMove.equals("scissors")){
                 System.out.println("I don't think that's a move you can make in rock paper scissors my guy :/");
             }else {
-//                String[] random = {"rock", "paper", "scissors"};
                 //Get a random number in between 0 and 3 and convert it to an integer so that the possibilities are 0, 1, or 2
                 int rand = (int)(Math.random()*3);
                 //Convert the random number to a string using conditionals and print the opponent's move
@@ -66,19 +64,28 @@ public class menu {
     }
 
 
-    public static void main(String[] args) throws Exception {
+    public static void gameChoice() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the game you would like to play (we only have rock paper scissors or flipping a coin atm sorry) \n1. Rock paper scissors \n2. Coin flip \n3. Quit");
+        int gameNo = in.nextInt();
+        if (gameNo == 1){
+            rockPaperScissors(gameNo);
+        }
+        else if (gameNo == 2) {
+            coinFlip(gameNo);
+        }
+        else if (gameNo == 3) {
+            System.out.println("See you next time :)");
+        }
+    }
+
+
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Hello! \n 1. Start \n 2. Quit ");
         int start = input.nextInt();
         if (mainMenu(start)) {
-            System.out.println("Enter the game you would like to play (we only have rock paper scissors or flipping a coin atm sorry) \n 1. Rock paper scissors \n 2. Coin flip");
-            int gameChoice = input.nextInt();
-            if (gameChoice == 1){
-                rockPaperScissors(gameChoice);
-            }
-            else if (gameChoice == 2) {
-                coinFlip(gameChoice);
-            }
+            gameChoice();
         }else{
             System.out.println("See you next time :)");
         }
